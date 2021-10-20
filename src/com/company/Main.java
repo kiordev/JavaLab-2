fc6c3ad70e5f930b3e73f59eaa35d0de1b1a8d4e
kiordev <kiordev@gmail.com>
2021-10-20 14:40:27 +0300

    Тестирование

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -106,6 +106,7 @@ public class Main {
     static void Task_Six(){
         System.out.println("Проверка гита");
         System.out.println("Проверка гита");
+        System.out.println("Проверка гита");
     } //Шестое задание
     static void Task_Seven(){
         System.out.println("Проверка гита");
